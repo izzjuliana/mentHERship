46de96809e40e2471a20a46253c94155fb14a22f
Alaena <alaena1211@gmail.com>
2025-01-25 23:05:59 -0500

    merge again lol

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import './styles.css';
+import './Components/styles.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
